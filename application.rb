0f45bdc4b8565a5719c63d401658ab5601d83ccb
Pierre Legoux <pierrelegoux@MacBook-Pro-de-Pierre.local>
2022-07-04 12:05:43 +0200

    bug fixing

--- application.rb
+++ application.rb
@@ -1,9 +1,11 @@
 # require "bundler"
 # Bundler.require
 
+
 $:.unshift File.expand_path("../lib", __FILE__)
 
 require_relative "lib/router"
 require_relative "lib/controller"
+
 
 Router.new.perform
